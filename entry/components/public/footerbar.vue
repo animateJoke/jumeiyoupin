22c9fba77253efcec69367a7332c8436500a0dff
animate <1299969195@qq.com>
2018-03-30 21:01:22 +0800

    搭建页面结构

---
+++ entry/components/public/footerbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="footer">
+        <ul>
+            <li>
+                <a href="#/home/index">首页</a>
+            </li>
+            <li>
+                <a href="#/home/group">拼团</a>
+            </li>
+            <li>
+                <a href="#/home/cart">购物车</a>
+            </li>
+            <li>
+                <a href="#/home/center">我的</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped="">
+    .footer{
+        position:fixed;
+        bottom:0;
+        left: 0;
+        height:50px;
+        background-color:#fff;
+        width:100%;
+        border-top:1px solid #ccc;
+    }
+    .footer ul{
+        display:flex;
+        width:100%;
+        height:100%;
+    }
+    .footer ul li{
+        flex:1;
+        line-height:50px;
+        text-align:center;
+    }
+</style>
--- entry/entry.js
+++ entry/entry.js
@@ -1,17 +1,49 @@
 import Vue from "vue";
+
+require("./css/reset.css");
 //状态管理
 import Vuex from 'vuex'
-Vue.use(Vuex)
+
+Vue.use(Vuex);
 //路由管理
 import VueRouter from 'vue-router'
+
 Vue.use(VueRouter);
 
+
+import home from "./components/home.vue";
+
+import index from "./components/home/index.vue";
+import group from "./components/home/group.vue";
+import cart from "./components/home/cart.vue";
+import center from "./components/home/center.vue";
+
+const router = new VueRouter({
+    routes : [{
+        path : "/home",
+        component : home,
+        children : [{
+            path : "index",
+            component : index
+        }, {
+            path : "group",
+            component : group
+        }, {
+            path : "cart",
+            component : cart
+        }, {
+            path : "center",
+            component : center
+        }]
+    }]
+});
 new Vue({
     el : "#app",
     template : `
         <div>
-            
-        
+            <router-view></router-view>
         </div>
-    `
-})
+    `,
+    router
+
+});
