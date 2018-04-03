0c7bbe99e4c698c32a7bf100572c546a06cca00e
ssty <496927765@qq.com>
2018-04-04 07:43:44 +0800

    小修改

--- entry/components/home/cart.vue
+++ entry/components/home/cart.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
+    <div class="cart">
+        <div class="head">
+
+        </div>
     </div>
 </template>
 
@@ -7,6 +10,13 @@
 
 </script>
 
-<style>
-
+<style scoped>
+    .cart{
+        border: 1px solid #e3e3e4;
+        box-sizing: border-box;
+    }
+    .head{
+        height: .41rem;
+        border-bottom: 1px solid #e3e3e4;
+    }
 </style>
