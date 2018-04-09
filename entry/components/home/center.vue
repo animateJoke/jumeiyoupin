c6a097cc40cd1eaee38ae63eef3b6c41f8da6423
threewish <1159285669@qq.com>
2018-04-09 15:38:58 +0800

    登录注册

--- entry/components/home/center.vue
+++ entry/components/home/center.vue
@@ -1,23 +1,6 @@
 <template>
 	<div style="width: 100%;height: 100%;background: #f2f2f2;">
-		<div class="header">
-			<a href="#">
-				<</a>
-					<a href="#">我的聚美</a>
-					<i class="iconfont">&#xe66c;</i>
-		</div>
-		<div class="login">
-			<img src="../../img/default.png" />
-			<ul>
-				<li>
-					<a href="#/register">注册</a>
-				</li>
-				<li>|</li>
-				<li>
-					<a href="#/login">登录</a>
-				</li>
-			</ul>
-		</div>
+		<xheader/>
 		<div class="goods">
 			<div class="goods_top">
 				<i class="iconfont">&#xe648;</i>
@@ -124,67 +107,16 @@
 </template>
 
 <script>
+	 import xheader from "./header.vue";
+    export default {
+        components:{
+            xheader
+        }
+    }
 </script>
 
 <style scoped="scoped">
-	.header {
-		width: 100%;
-		height: 0.38rem;
-		background: #fd4d71;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		font-size: 0.16rem;
-		color: white;
-	}
 	
-	.header i {
-		margin-right: 0.1rem;
-		font-size: 0.30rem;
-	}
-	
-	.header a {
-		text-decoration: none;
-		color: white;
-	}
-	
-	.header a:first-child {
-		margin-left: 0.1rem;
-		font-size: 0.30rem;
-	}
-	
-	.login {
-		width: 100%;
-		height: 1.58rem;
-		background: #fd4d71;
-		margin-top: -0.01rem;
-		text-align: center;
-	}
-	
-	.login img {
-		width: 0.7rem;
-		margin-top: 0.3rem;
-	}
-	
-	.login ul {
-		width: 100%;
-		display: flex;
-		margin-top: 0.15rem;
-		justify-content: center;
-	}
-	
-	.login ul li {
-		width: 0.5rem;
-		float: left;
-		font-size: 0.2rem;
-		font-weight: 600;
-		color: white;
-	}
-	
-	.login ul li a {
-		text-decoration: none;
-		color: white;
-	}
 	
 	.goods {
 		width: 100%;
---
+++ entry/components/home/header.vue
@@ -0,0 +1,119 @@
+<template>
+	<div>
+		<div class="header">
+			<a href="#">
+				<</a>
+					<a href="#">我的聚美</a>
+					<i class="iconfont">&#xe66c;</i>
+	</div>
+	<div class="view">
+		<img src="../../img/default.png" />
+		<p>14785789751</p>
+		<span>普通会员</span>
+	</div>
+	<div class="item">
+		<ul>
+			<li>
+				<img src="../../img/wishlist.png" />
+				<p>心愿单</p>
+			</li>
+			<li><img src="../../img/onsale.png" />
+				<p>开售提醒</p>
+			</li>
+			<li>
+				<img src="../../img/fav.png" />
+				<p>收藏</p>
+			</li>
+		</ul>
+	</div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style scoped="scoped">
+	.header {
+		width: 100%;
+		height: 0.38rem;
+		background: #fd4d71;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		font-size: 0.16rem;
+		color: white;
+	}
+	
+	.header i {
+		margin-right: 0.1rem;
+		font-size: 0.30rem;
+	}
+	
+	.header a {
+		text-decoration: none;
+		color: white;
+	}
+	
+	.header a:first-child {
+		margin-left: 0.1rem;
+		font-size: 0.30rem;
+	}
+	.view {
+		width: 100%;
+		height: 0.84rem;
+		background: #fd4d71;
+		margin-top: -0.01rem;
+		position: relative;
+	}
+	
+	.view img {
+		position: absolute;
+		width: 0.7rem;
+		top: 0.5rem;
+		left: 0.38rem;
+	}
+	.view p{
+		float: left;
+		margin-top: 0.4rem;
+		margin-left: 1.3rem;
+		font-size: 0.14rem;
+		color: white;
+	}
+	.view span{
+		display: block;
+		width: 0.7rem;
+		height: 0.21rem;
+		line-height: 0.21rem;
+		text-align: center;
+		border-radius: 0.1rem;
+		float: left;
+		margin-top: 0.6rem;
+		margin-left: -0.8rem;
+		font-size: 0.14rem;
+		background: #fe779e;
+		color: white;
+	}
+	.item{
+		width: 100%;
+		height: 0.74rem;
+		background: white;
+		
+	}
+	.item ul li{
+		float: left;
+		width: 0.81rem;
+		height: 0.74rem;
+		font-size: 0.14rem;
+		text-align: center;
+	}
+	.item ul li:first-child{
+		margin-left: 1.3rem;
+	}
+	.item ul li img{
+		width: 0.25rem;
+		margin-top: 0.1rem;
+	}
+	.item ul li p{
+		margin-top: 0.1rem;
+	}
+</style>
